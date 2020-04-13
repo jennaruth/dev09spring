209dcae77c2d3284e54d4d548fed1cbc82e5a87b
jennaruth <47092497+jennaruth@users.noreply.github.com>
2020-04-13 16:45:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function () {
+  console.log("ready!");
+ 
+
+ $('#sender').on('click', function (e) {
+e.preventDefault();
+var getInput = $('#textInput').val(); 
+   
+$('#textOutput').append('<p>'+getInput+'</p>'); 
+
+});
+});
